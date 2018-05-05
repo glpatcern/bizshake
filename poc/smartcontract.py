769889523f9f61c3ad8363be98b069dc2066cee5
Giuseppe Lo Presti <glopresti@gmail.com>
2018-05-05 16:20:25 +0200

    First edition of a working SC

--- poc/smartcontract.py
+++ poc/smartcontract.py
@@ -12,26 +12,27 @@ from boa.builtins import concat
 #from smartrent import SmartRent
 #from smartpawn import SmartPawn
 
-rentOps = ['NewAsset', 'GetAsset', 'DeleteAsset']
-pawnOps = []
-
+storageOps = ['Store', 'Retrieve', 'Delete']
+BZSVERSION = '0.4'
 
 """Helper functions to augment SC storage with a double key"""
 
 def put(category, id, value):
     skey = concat(category, id)
     Put(GetContext(), skey, value)
-        
-def get(self, category, id):
+    
+def get(category, id):
     skey = concat(category, id)
     v = Get(GetContext(), skey)
     if v == 0:
         return None
     return v
 
-def delete(self, category, id):
+# XXX Currently broken
+def delete(category, id):
     skey = concat(category, id)
-    return Delete(GetContext(), skey)
+    Delete(GetContext(), skey)
+    #Log(r)
 
 
 def Main(operation, args):
@@ -40,32 +41,48 @@ def Main(operation, args):
     :param args: list A list of arguments along with the operation
     """
 
-    # Am I who I say I am?
-    user_hash = args[0]
-    authorized = CheckWitness(user_hash)
+    if operation == 'GetVersion':
+        # very simple method to test the contract
+        Log("Invoked GetVersion")
+        return BZSVERSION
+
+    # Am I who I say I am? args[0] musth be the wallet hash code
+    authorized = CheckWitness(args[0])
     if not authorized:
         Log("Not Authorized")
         return 'Not Authorized'
+    Log(args[0])
 
-    # dispatch
-    key = args[1]
-    if operation == 'NewAsset':
-        put('ASSET', key, args[2])
-        output = concat("PutAsset stored ", args[2])
-        print(output)
+    # storage operations
+    if operation == 'Store':
+        if len(args) < 4:
+            return 'Invalid arguments'
+        category = args[1]
+        id = args[2]
+        put(category, id, args[3])   # args[3] = arbitrary payload
+        Notify(['Store:', category, id])
         return 'OK'
 
-    if operation == 'GetAsset':
-        assetData = get('ASSET', key)
-        output = concat("GetAsset got ", assetData)
-        print(output)
-        return assetData
+    if operation == 'Retrieve':
+        if len(args) < 3:
+            return 'Invalid arguments'
+        category = args[1]
+        id = args[2]
+        skey = concat(category, id)
+        data = Get(GetContext(), skey)
+        #data = get(category, id)
+        Notify(['Retrieve:', category, id])
+        return data
 
-    if operation == 'DeleteAsset':
-        res = delete('ASSET', key)
-        output = concat("DeleteAsset got ", res)
-        print(output)
-        return res
+    if operation == 'Delete':
+        if len(args) < 3:
+            return 'Invalid arguments'
+        category = args[1]
+        id = args[2]
+        skey = concat(category, id)
+        Delete(GetContext(), skey)
+        Notify(['Delete:', category, id])
+        return 'OK'
 
 #    if operation in pawnOps:
 #        print("SmartPawn operation")
